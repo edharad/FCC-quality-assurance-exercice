9e1f2a8dc78fc2bc8135d30684941cec47c4ef9f
KHUD <egzon.d.haradinaj@gmail.com>
2024-08-21 15:25:56 +0200

    fix/final-server

--- server.js
+++ server.js
@@ -85,8 +85,8 @@ app.get('/_api/get-tests', cors(), function (req, res, next) {
   });
 
 
-const port = process.env.PORT || 3000;
-app.listen(port, '127.0.0.1', function () {
+const port = process.env.PORT || 4000;
+app.listen(port, function () {
   console.log("Listening on port " + port);
   console.log('Running Tests...');
   setTimeout(function () {
